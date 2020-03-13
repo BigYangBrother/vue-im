54a77746e6c8114b6e7f674dc57af65fd8500d02
liuyang <1126323596@qq.com>
2020-03-13 16:02:16 +0800

    修改sh

--- test.sh
+++ test.sh
@@ -4,16 +4,23 @@ branch1=$1
 branch2=$2
 # 环境
 dev=$3
+# 提交日志
+message=$4
 # 切分支拉代码
-git checkout ${branch}
+git checkout ${branch1}
 git pull
 # build
 npm run build:${dev}
 # 进入后端仓库
 cd /Users/liuyang/personal/build-test
 git pull
+git checkout ${branch2}
 # 复制文件
 filea=/Users/liuyang/personal/vue-im/src/main.js
 cp $filea ./
+# 提交代码
+git add .
+git commit -m ${message}
+git push
 
 
